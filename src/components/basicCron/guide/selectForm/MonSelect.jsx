b982495eb9800e7ed064329502b8c543e1c505cf
shawn <shawn@salin.co.kr>
2020-11-16 18:54:42 +0900

    commit

--- src/components/basicCron/guide/selectForm/MonSelect.jsx
+++ src/components/basicCron/guide/selectForm/MonSelect.jsx
@@ -2,6 +2,40 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from '../../../../styles.module.css'
 
+const Test = () => {
+  return (
+    <div className=''>
+      <button className='' type='button' title='Clear'>
+        <span className=''>
+          <svg className=''>
+            <path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z' />
+          </svg>
+        </span>
+        <span className='MuiTouchRipple-root' />
+      </button>
+      <button
+        className='MuiButtonBase-root MuiIconButton-root MuiAutocomplete-popupIndicator'
+        tabIndex='-1'
+        type='button'
+        aria-label='Open'
+        title='Open'
+      >
+        <span className='MuiIconButton-label'>
+          <svg
+            className='MuiSvgIcon-root'
+            focusable='false'
+            viewBox='0 0 24 24'
+            aria-hidden='true'
+          >
+            <path d='M7 10l5 5 5-5z' />
+          </svg>
+        </span>
+        <span className='MuiTouchRipple-root' />
+      </button>
+    </div>
+  )
+}
+
 const createArrWithNum = (num) => {
   const arr = []
   for (let i = 0; i < num; i++) {
@@ -22,15 +56,26 @@ const MonSelect = ({ select, handleChange }) => {
   const { mon } = select
   const res = createArrWithNum(12)
   return (
-    <div className={styles.select__container}>
-      <select defaultValue={mon} onChange={handleChange} id='mon'>
-        {res.map((item) => (
-          <option key={item.id} value={item.value}>
-            {item.label}
-          </option>
-        ))}
-      </select>
-    </div>
+    <React.Fragment>
+      <div role='combobox'>
+        <div className='MuiFormControl-root MuiTextField-root MuiFormControl-fullWidth'>
+          <label />
+
+          <div>
+            <input type='text' />
+            <Test />
+            <fieldset
+              aria-hidden='true'
+              className='PrivateNotchedOutline-root-1 MuiOutlinedInput-notchedOutline'
+            >
+              <legend className='PrivateNotchedOutline-legendLabelled-3'>
+                <span>Checkboxes</span>
+              </legend>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
 
