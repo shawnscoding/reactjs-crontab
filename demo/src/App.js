b9cc389d4048c24a3913ba1473d3ddcf479da7c4
shawn <shawn@salin.co.kr>
2020-12-03 11:23:43 +0900

    ui bug fixes -- no utc indicator and removed consoles

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { CronGuide,BasicCron } from 'reactjs-crontab'
+import { CronGuide, BasicCron } from 'reactjs-crontab'
 import 'reactjs-crontab/dist/index.css'
 // simply import CronGuide and css and that's all.
 
 const App = () => {
   return (
     <div>
-      <BasicCron />
       <CronGuide />
 
     </div>
